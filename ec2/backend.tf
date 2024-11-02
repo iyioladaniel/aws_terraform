9db8d95024c12e5b0a3bf56a6f58af7392f0a44c
Daniel Iyiola <iyiola_daniel@yahoo.com>
2024-11-02 13:51:44 +0100

    remove dynamodb from statefile & added provider.tf

--- ec2/backend.tf
+++ ec2/backend.tf
@@ -3,7 +3,6 @@ terraform {
     bucket         = "remote-state-bucket"
     key            = "state-files/terraform.tfstate"
     region         = "eu-north-1"
-    dynamodb_table = "terraform-lock-table" # For state locking
     encrypt        = true
   }
 }
