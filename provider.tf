ea7ec499002efe5fb31a2285f207b4d1a1332bc0
Daniel Iyiola <iyiola_daniel@yahoo.com>
2024-11-02 15:46:49 +0100

    added route table associations to subnets

--- provider.tf
+++ provider.tf
@@ -1,3 +1,14 @@
+# provider.tf
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"  # Use appropriate version constraint
+    }
+  }
+  required_version = ">= 1.2.0"  # Specify minimum Terraform version
+}
+
 provider "aws" {
-    region = "eu-north-1"
+  region = "eu-north-1"
 }
--- vpc/main.tf
+++ vpc/main.tf
@@ -1,83 +1,110 @@
-provider "aws" {
-    region = "eu-north-1"
-}
-
 #Create vpc
-resource "aws_vpc" "practice" {
+resource "aws_vpc" "practice_vpc" {
     cidr_block = "10.0.0.0/16"
 
     tags = {
-        Name = "practice-vpc"
+        Name = "practice_vpc"
     }
 }
 
 #Create subnets
-resource "aws_subnet" "practice_private_subnet" {
-    vpc_id = aws_vpc.practice.id
+resource "aws_subnet" "private_subnet" {
+    vpc_id = aws_vpc.practice_vpc.id
     cidr_block =  "10.0.0.0/24"
 
     tags = {
-        Name = "practice-private-vpc"
+        Name = "private_subnet",
+        vpc = "practice_vpc"
     }
 }
 
-resource "aws_subnet" "practice_public_subnet" {
-    vpc_id = aws_vpc.practice.id
+resource "aws_subnet" "public_subnet" {
+    vpc_id = aws_vpc.practice_vpc.id
     cidr_block =  "10.0.1.0/24"
 
     tags = {
-        Name = "practice-public-vpc"
+      Name = "public_subnet" 
+      vpc = "practice_vpc"
     }
 }
 
 #Create internet gateway
-resource "aws_internet_gateway" "practice_internet_gw" {
-  vpc_id = aws_vpc.practice.id
+resource "aws_internet_gateway" "internet_gateway" {
+  vpc_id = aws_vpc.practice_vpc.id
 
   tags = {
-    Name = "practice-internet-gateway"
+    Name = "internet_gateway",
+    vpc = "practice_vpc"
+  }
+}
+
+#Create Elastic IP for NAT Gateway
+resource "aws_eip" "eip_nat_gateway" {
+  domain = "vpc"
+  depends_on = [aws_internet_gateway.internet_gateway]
+  tags = {
+    Name = "nat_gateway_elastic_ip"
+    vpc = "practice_vpc"
   }
 }
 
 #Create public NAT gateway
-resource "aws_nat_gateway" "practice-public-nat-gw" {
-  subnet_id     = aws_subnet.practice_public_subnet.id
-
-  tags = {
-    Name = "practice-public-gateway"
-  }
+resource "aws_nat_gateway" "nat_gateway" {
+  subnet_id     = aws_subnet.public_subnet.id
+  allocation_id = aws_eip.eip_nat_gateway.id
 
   # To ensure proper ordering, it is recommended to add an explicit dependency
   # on the Internet Gateway for the VPC.
-  depends_on = [aws_internet_gateway.practice_internet_gw]
+  depends_on = [aws_internet_gateway.internet_gateway]
+
+  tags = {
+    Name = "nat_gateway",
+    subnet = "public_subnet",
+    vpc = "practice_vpc"
+  }
 }
 
 #Create route tables
-resource "aws_route_table" "practice_public_route_table" {
-  vpc_id = aws_vpc.practice.id
+resource "aws_route_table" "public_route_table" {
+  vpc_id = aws_vpc.practice_vpc.id
 
   route {
     cidr_block = "0.0.0.0/0" 
-    gateway_id = aws_internet_gateway.practice_internet_gw.id
+    gateway_id = aws_internet_gateway.internet_gateway.id
   }
 
-
   tags = {
-    Name = "practice-public-route-table"
+    Name = "public_route_table"
+    subnet = "public_subnet"
+    gateway = "nat_gateway"
+    subnet = "private_subnet"
   }
 }
 
 
-resource "aws_route_table" "practice_private_route_table" {
-  vpc_id = aws_vpc.practice.id
+resource "aws_route_table" "private_route_table" {
+  vpc_id = aws_vpc.practice_vpc.id
 
   route {
     cidr_block = "0.0.0.0/0" 
-    gateway_id = aws_nat_gateway.practice-public-nat-gw.id 
+    gateway_id = aws_nat_gateway.nat_gateway.id 
   }
-
 
   tags = {
-    Name = "practice-private-route-table"
+    name = "private-route-table"
+    vpc = "private_vpc"
+    gateway = "nat_gateway"
+    subnet = "private_subnet"
   }
+}
+
+#Create route table associations between subnets and route tables
+resource "aws_route_table_association" "public_rt_association" {
+  subnet_id = aws_subnet.public_subnet.id
+  route_table_id = aws_route_table.public_route_table.id
+}
+
+resource "aws_route_table_association" "private_rt_association" {
+  subnet_id = aws_subnet.private_subnet.id
+  route_table_id = aws_route_table.private_route_table.id
 }
