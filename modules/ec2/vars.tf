56e51eb2c6f8796caa92551e094ef2d6632871c0
King-Juliet <chibuokejuliet@gmail.com>
2024-11-05 08:26:46 +0100

    added ec2.tf

--- modules/ec2/vars.tf
+++ modules/ec2/vars.tf
@@ -3,8 +3,13 @@ variable "id_of_vpc"{
 }
 
 
-variable "security_group_name"{
-    description = "name of security group"
+variable "public_security_group_name"{
+    description = "name of public security group"
+    default = "security group"
+}
+
+variable "private_security_group_name"{
+    description = "name of private security group"
     default = "security group"
 }
 
@@ -18,8 +23,24 @@ variable "ec2_sg_environment" {
   default = "dev"
 }
 
-variable "sg_inbound_traffic"{
-    description = "permissible inbound IP addresses to the ec2 instance"
+variable "public_sg_inbound_traffic"{
+    description = "permissible inbound IP addresses to the public ec2 instance"
+    default = "0.0.0.0/0"
+}
+
+variable "public_sg_outbound_traffic"{
+    description = "permissible outbound IP addresses to the public ec2 instance"
+    default = "0.0.0.0/0"
+}
+
+
+variable "private_sg_inbound_traffic"{
+    description = "permissible inbound IP addresses to the private ec2 instance"
+    default = "0.0.0.0/0"
+}
+
+variable "private_sg_outbound_traffic"{
+    description = "permissible outbound IP addresses to the private ec2 instance"
     default = "0.0.0.0/0"
 }
 
@@ -33,28 +54,52 @@ variable "port_number"{
     default = "22"
 }
 
-variable "ssh_keyname"{
-    description = "name given to the ssh key created"
-    default = "ec2 ssh key"
+variable "public_ssh_keyname"{
+    description = "name given to the ssh key created for public ec2 "
+    default = "public-ec2-ssh-key"
 }
 
-variable "ssh_filepath"{
-    description = "file path to the ssh key created"
+variable "public_ssh_filepath"{
+    description = "file path to the ssh key created for public ec2 instance"
     default = "ec2 ssh key file path for private key"
 }
 
-variable "ec2_ami"{
-    description = "operating system of ec2"
+#variable "private_ssh_keyname"{
+    #description = "name given to the ssh key created for private ec2 "
+    #default = "public-ec2-ssh-key"
+#}
+
+#variable "private_ssh_filepath"{
+    #description = "file path to the ssh key created for private ec2 instance"
+    #default = "ec2 ssh key file path for private key"
+#}
+
+variable "public_ec2_ami"{
+    description = "operating system of public ec2"
     default = "ami-08ec94f928cf25a9d"
 }
 
-variable "ec2_instance_type"{
-    description = "hardware to use for the ec2"
+variable "public_ec2_instance_type"{
+    description = "hardware to use for the public ec2"
     default = "t2.micro"
 }
 
-variable "ec2_instance_name"{
-    description = "name of the created ec2"
+variable "private_ec2_ami"{
+    description = "operating system of private ec2"
+    default = "ami-08ec94f928cf25a9d"
+}
+
+variable "private_ec2_instance_type"{
+    description = "hardware to use for the private ec2"
+    default = "t2.micro"
+}
+
+variable "public_ec2_instance_name"{
+    description = "name of the created public ec2"
+}
+
+variable "private_ec2_instance_name"{
+    description = "name of the created private ec2"
 }
 
 variable "ec2_instance_owner"{
